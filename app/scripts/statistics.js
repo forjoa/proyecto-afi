37e503dbb500272029d79df62f0dd11aec857cc7
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-01-25 19:39:39 +0100

    Finish statistics header

---
+++ app/scripts/statistics.js
@@ -0,0 +1,11 @@
+const burguerMenu = document.getElementById('menu')
+const navbar = document.getElementById('navbar')
+const logout = document.getElementById('logout-btn')
+
+burguerMenu.addEventListener('click', () => {
+    navbar.classList.toggle('hidden')
+})
+
+logout.addEventListener('click', () => {
+    window.location.href = '/app/'
+})
