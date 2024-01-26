519e7e32c7962d76e7a6602e83c7df93c2596790
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-01-26 12:30:27 +0100

    Almost finish statistics app

--- app/scripts/statistics.js
+++ app/scripts/statistics.js
@@ -1,6 +1,8 @@
 const burguerMenu = document.getElementById('menu')
 const navbar = document.getElementById('navbar')
 const logout = document.getElementById('logout-btn')
+const ctx = document.getElementById('myChart')
+const ctx2 = document.getElementById('myChart2')
 
 burguerMenu.addEventListener('click', () => {
   navbar.classList.toggle('hidden')
@@ -8,4 +10,59 @@ burguerMenu.addEventListener('click', () => {
 
 logout.addEventListener('click', () => {
   window.location.href = '/app/'
+})
+
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio'],
+    datasets: [
+      {
+        label: 'Cantidad de ventas',
+        data: [12, 19, 31, 5, 19, 13],
+        backgroundColor: [
+          '#1C6994',
+        ],
+        borderWidth: 0,
+        barPercentage: 0.5,
+        barThickness: 40,
+        maxBarThickness: 40,
+        minBarLength: 2,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+})
+
+new Chart(ctx2, {
+  type: 'line',
+  data: {
+    labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio'],
+    datasets: [
+      {
+        label: 'Cereales',
+        data: [12, 30, 9, 32, 27, 1],
+        backgroundColor: '#00BE62',
+        borderColor: '#00BE62'
+      },
+      {
+        label: 'Bebidas',
+        data: [23, 43, 12, 46, 19, 10],
+        backgroundColor: '#1C6994',
+        borderColor: '#1C6994',
+      },
+      {
+        label: 'Snacks',
+        data: [10, 20, 30   , 40, 10, 19],
+        backgroundColor: '#7B97AA',
+        borderColor: '#7B97AA',
+      },
+    ],
+  },
 })
