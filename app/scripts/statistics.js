d330cf027f8a2352f2b36dab51dcd79c5a2df5b2
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-01-26 09:05:10 +0100

    Add first graphic statistics

--- app/scripts/statistics.js
+++ app/scripts/statistics.js
@@ -3,9 +3,9 @@ const navbar = document.getElementById('navbar')
 const logout = document.getElementById('logout-btn')
 
 burguerMenu.addEventListener('click', () => {
-    navbar.classList.toggle('hidden')
+  navbar.classList.toggle('hidden')
 })
 
 logout.addEventListener('click', () => {
-    window.location.href = '/app/'
+  window.location.href = '/app/'
 })
