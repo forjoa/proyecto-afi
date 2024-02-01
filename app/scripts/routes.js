79b709466caaff0d2265667672c1913c74a6c290
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-02-01 13:46:56 +0100

    changed locations in madrid

--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -13,112 +13,42 @@ logout.addEventListener('click', () => {
 const CONFIGURATION = {
   locations: [
     {
-      title: 'Escondido Grill',
-      address1: '601 Geary St',
-      address2: 'San Francisco, CA 94102, USA',
-      coords: { lat: 37.7866, lng: -122.4133 },
-      placeId: 'ChIJ5Sz52pGAhYAR1raOybzuDp8',
+      title: 'La Plaza Restaurante',
+      address1: 'Plaza Mayor, 1',
+      address2: '28012 Madrid, Spain',
+      coords: { lat: 40.41552, lng: -3.707157 },
+      placeId: 'ChIJSRMekDMoQg0R28uNfqwXJX0',
       actions: [
         {
-          label: 'Reserve a table',
+          label: 'Reservar una mesa',
           defaultUrl:
             'https://www.gstatic.com/pantheon/images/maps/reserve_with_google_preview_v1.png',
         },
       ],
     },
     {
-      title: 'Escondido Grill',
-      address1: '747 3rd St',
-      address2: 'San Francisco, CA 94107, USA',
-      coords: { lat: 37.7785, lng: -122.392 },
-      placeId: 'ChIJeyVKpNd_j4ARLUED10OEr1s',
+      title: 'El Retiro Tapas',
+      address1: 'Calle de Alcalá, 78',
+      address2: '28009 Madrid, Spain',
+      coords: { lat: 40.419305, lng: -3.681816 },
+      placeId: 'ChIJhQy2DG4oQg0RqkzHE3LMZG8',
       actions: [
         {
-          label: 'Reserve a table',
+          label: 'Reservar una mesa',
           defaultUrl:
             'https://www.gstatic.com/pantheon/images/maps/reserve_with_google_preview_v1.png',
         },
       ],
     },
     {
-      title: 'Escondido Grill',
-      address1: '1512 Stockton St',
-      address2: 'San Francisco, CA 94133, USA',
-      coords: { lat: 37.7998, lng: -122.4089 },
-      placeId: 'ChIJgYMdZvGAhYAR2ifWKOR5ek8',
+      title: 'Mercado San Miguel',
+      address1: 'Plaza de San Miguel, s/n',
+      address2: '28005 Madrid, Spain',
+      coords: { lat: 40.415491, lng: -3.708508 },
+      placeId: 'ChIJD4pSXDMoQg0RJEqyBz9L21I',
       actions: [
         {
-          label: 'Reserve a table',
-          defaultUrl:
-            'https://www.gstatic.com/pantheon/images/maps/reserve_with_google_preview_v1.png',
-        },
-      ],
-    },
-    {
-      title: 'Escondido Grill',
-      address1: '50 Clement St',
-      address2: 'San Francisco, CA 94118, USA',
-      coords: { lat: 37.7833, lng: -122.46 },
-      placeId: 'ChIJk_FWiDmHhYARgDVidBVO_RM',
-      actions: [
-        {
-          label: 'Reserve a table',
-          defaultUrl:
-            'https://www.gstatic.com/pantheon/images/maps/reserve_with_google_preview_v1.png',
-        },
-      ],
-    },
-    {
-      title: 'Escondido Grill',
-      address1: '3755 Noriega St',
-      address2: 'San Francisco, CA 94122, USA',
-      coords: { lat: 37.7529, lng: -122.5042 },
-      placeId: 'ChIJ_2knd4-HhYARI5PPNZ7aasA',
-      actions: [
-        {
-          label: 'Reserve a table',
-          defaultUrl:
-            'https://www.gstatic.com/pantheon/images/maps/reserve_with_google_preview_v1.png',
-        },
-      ],
-    },
-    {
-      title: 'Escondido Grill',
-      address1: '2619 Mission St',
-      address2: 'San Francisco, CA 94110, USA',
-      coords: { lat: 37.7552, lng: -122.4184 },
-      placeId: 'ChIJSwWfLT9-j4ARMGcgMs-tBRE',
-      actions: [
-        {
-          label: 'Reserve a table',
-          defaultUrl:
-            'https://www.gstatic.com/pantheon/images/maps/reserve_with_google_preview_v1.png',
-        },
-      ],
-    },
-    {
-      title: 'Escondido Grill',
-      address1: '4935 Mission St',
-      address2: 'San Francisco, CA 94112, USA',
-      coords: { lat: 37.7194, lng: -122.4386 },
-      placeId: 'ChIJtYdd3IR-j4ARSybFjVnGTBw',
-      actions: [
-        {
-          label: 'Reserve a table',
-          defaultUrl:
-            'https://www.gstatic.com/pantheon/images/maps/reserve_with_google_preview_v1.png',
-        },
-      ],
-    },
-    {
-      title: 'Escondido Grill',
-      address1: '6202 3rd St',
-      address2: 'San Francisco, CA 94124, USA',
-      coords: { lat: 37.7223, lng: -122.396 },
-      placeId: 'ChIJ0VRZVB9_j4ARaB7r66H67eg',
-      actions: [
-        {
-          label: 'Reserve a table',
+          label: 'Reservar una mesa',
           defaultUrl:
             'https://www.gstatic.com/pantheon/images/maps/reserve_with_google_preview_v1.png',
         },
@@ -126,11 +56,11 @@ const CONFIGURATION = {
     },
   ],
   mapOptions: {
-    center: { lat: 38.0, lng: -100.0 },
+    center: { lat: 40.416775, lng: -3.70379 },
     fullscreenControl: true,
     mapTypeControl: false,
     streetViewControl: false,
-    zoom: 4,
+    zoom: 12,
     zoomControl: true,
     maxZoom: 17,
     mapId: '',
